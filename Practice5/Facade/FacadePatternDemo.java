4bbec5bcbcd233f8af428a07e34a5b544b81aa7c
Obolonsky Denis <denis.obolonsky@gmail.com>
2022-10-12 23:43:17 +0300

    homework5

--- Practice5/Facade/FacadePatternDemo.java
+++ Practice5/Facade/FacadePatternDemo.java
@@ -7,6 +7,7 @@ import Practice5.Facade.Shape.Square;
 public class FacadePatternDemo {
     public static void main(String[] args) {
         ShapeMaker shapeMaker = new ShapeMaker();
+
         Circle circle = shapeMaker.makeAndPrepareCircle("circleTexture.dds");
         circle.render();
         System.out.println();
