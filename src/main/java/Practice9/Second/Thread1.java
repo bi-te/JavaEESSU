76b125f59e2519126942c087f78946821cc0d64d
Obolonsky Denis <d.obolonskyi@dragonslake.com>
2022-11-30 15:52:11 +0200

    homework 9

--- src/main/java/Practice9/Second/Thread1.java
+++ src/main/java/Practice9/Second/Thread1.java
@@ -9,7 +9,7 @@ public class Thread1 implements Runnable {
 
     @Override
     public void run() {
-        {
+        synchronized (bank){
             for (int i = 0; i < 20000; i++) {
                 bank.setAccount(bank.getAccount() + 2);
             }
